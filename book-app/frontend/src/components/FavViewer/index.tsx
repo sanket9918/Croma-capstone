39453e970bd3d4b0d818c64e76e65e9b6cd92a75
Sanket <sanketmohapatra13@gmail.com>
2022-08-19 08:54:56 +0530

    Removed reload logic

--- book-app/frontend/src/components/FavViewer/index.tsx
+++ book-app/frontend/src/components/FavViewer/index.tsx
@@ -10,7 +10,12 @@ function FavoriteViewer() {
   const [favList, setFavList] = useState<IFav[]>([
     { id: 0, book_name: "fetching" },
   ] as IFav[]);
+
+  /**
+   * Loading widget for the initial placeholder
+   */
   const [loading, setLoading] = useState(false);
+
   const config = {
     headers: {
       Authorization: `Bearer ${sessionStorage.getItem("key")}`,
@@ -18,20 +23,40 @@ function FavoriteViewer() {
       "Access-Control-Allow-Credentials": true,
     },
   };
-  useEffect(() => {
-    setLoading(true);
+
+  /**
+   * For deleting the items from the favourite list and relading the favorite items (reload cache)
+   * @param id 
+   */
+  const handleDeleteItem = (id: number) => {
+    axios.delete(`http://localhost:8000/deleteFavorite/${id}`, config).then(() => {
+      handleFavListLoad()
+    }).catch((e) => alert("Sorry fav book couldn't be deleted."))
+  }
+
+  /**
+   * Function for loading the favourite items from the DB
+   */
+  const handleFavListLoad = () => {
     axios
       .get(
         `http://localhost:8000/favoriteByUser/${sessionStorage.getItem("id")}`,
         config
       )
       .then((e) => {
-        console.log(e.data);
-
         setFavList(e.data);
         setLoading(false);
       })
       .catch((e) => console.log(e));
+  }
+
+  /**
+   * For initial page load
+   */
+  useEffect(() => {
+    setLoading(true);
+    handleFavListLoad();
+
   }, []);
 
   return (
@@ -44,19 +69,13 @@ function FavoriteViewer() {
             <p>Loading...</p>
           ) : (
             favList.map((e) => (
-              
+
               <div className="mx-auto">
                 <div className="flex justify-between items-center">
                   <h3 key={e.id} className="text-xl mb-3 font-bold">
                     - {e.book_name}
                   </h3>
-                  <button className="mb-5 p-2 bg-black text-white rounded-lg" onClick={(e1: React.SyntheticEvent) => {
-                    e1.preventDefault();
-                    axios.delete(`http://localhost:8000/deleteFavorite/${e.id}`, config).then((res) => {
-                      alert("Fav book is deleted")
-                      window.location.reload();
-                    }).catch((e) => alert("Sorry fav book couldn't be deleted."))
-                  }}>Delete</button>
+                  <button className="mb-5 p-2 bg-black text-white rounded-lg" onClick={() => handleDeleteItem(e.id)}>Delete</button>
                 </div>
               </div>
             ))
