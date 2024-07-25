907b6befc01a35bb381c94ac45f552b1a76f5241
Sanket Mohapatra <sanketmohapatra13@gmail.com>
2024-07-25 11:04:16 +0530

    feat(spring-upgarde): Full upgrade to spring 3 and added dependencies

--- book-app/backend/src/main/java/com/sanket/bookapp/security/SecurityConfig.java
+++ book-app/backend/src/main/java/com/sanket/bookapp/security/SecurityConfig.java
@@ -5,14 +5,11 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -35,8 +32,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-
+                .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests(
                         (auth) -> auth.requestMatchers("/auth/**").permitAll().anyRequest().authenticated())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
